d77b2745091e2963dcb30e788e18e30fdc4c5d90
jinal1681 <jinal1681@gmail.com>
2015-07-11 23:27:13 +0530

    login validation
    connect.php-> credentials of database  loginvalidate.php->validates
    login

---
+++ loginvalidate.php
@@ -0,0 +1,16 @@
+<?php
+include 'connect.php';
+
+$username = $_POST['user'];
+$type=$_POST['type'];
+$password = $_POST['password'];
+$query_validate = "select * from login where username = '".$username."' AND password = '".$password."' AND type= '".$type."'" ;
+$query_exec = mysql_query($query_validate) or die(mysql_error());
+$rows = mysql_num_rows($query_exec);
+ if($rows == 0) { 
+ echo include 'login.html';
+ }
+ else  {
+    echo "User Found" ;
+}
+?>
