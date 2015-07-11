d53aea2b174aae7bdc6e1be10a8117eacd2cac82
jinal1681 <jinal1681@gmail.com>
2015-07-11 23:20:24 +0530

    connect.php-->credentials for php

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+$hostname_localhost ="http://10.242.162.8/";
+$database_localhost ="hhf";
+$username_localhost ="";
+$password_localhost ="";
+$localhost = mysql_connect($hostname_localhost,$username_localhost,$password_localhost)
+or
+trigger_error(mysql_error(),E_USER_ERROR);
+ 
+mysql_select_db($database_localhost, $localhost);
+
+?>
