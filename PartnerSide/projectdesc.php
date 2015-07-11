4b0a6e8de01d0b1717ce9061cc405cb0fee36a85
Shraddha26 <shraddhalunkad2011@yahoo.in>
2015-07-11 23:09:30 +0530

    Partner Side
    consists the layout of the partner side  with options to input data

---
+++ PartnerSide/projectdesc.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+#header {
+    background-color:black;
+    color:white;
+    text-align:center;
+    padding:5px;
+}
+#nav {
+    line-height:30px;
+    background-color:#eeeeee;
+    height:300px;
+    width:100px;
+    float:left;
+    padding:5px;	      
+}
+#section {
+    width:350px;
+    float:left;
+    padding:10px;	 	 
+}
+#footer {
+    background-color:black;
+    color:white;
+    clear:both;
+    text-align:center;
+   padding:5px;	 	 
+}
+</style>
+</head>
+<body>
+
+<div id="header">
+<h1>Progress Report</h1>
+</div>
+
+<div id="nav">
+<button type = "button">Project 1</button><br>
+<button type = "button">Project 2</button><br>
+<button type = "button">Project 3</button><br>
+<button type = "button">Project 4</button><br>
+</div>
+
+<div id="section">
+<button onclick = "window.location.href='file:///C:/Users/Admin/Desktop/New%20folder/inputpart.html'">Detailed Information</button><br>
+<button onclick = "window.location.href='file:///C:/Users/Admin/Desktop/New%20folder/quickup.html'">Quick Updation</button><br>
+</div>
+
+<div id="footer">
+<input type = "Submit">
+</div>
+
+</body>
+</html>
---
+++ PartnerSide/quickupd.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+
+th, td {
+    padding: 15px;
+}
+</style>
+</head>
+<body>
+
+    <h>Description</h> <textarea name="text" cols=20 rows=3></textarea>
+    <br>
+    <h>Images</h> 
+    <br>
+    <h>Need more capital?</h>
+    <br>
+    <ul>
+	<li><button type="button">Yes</button></li>
+	<li><button type = "button">No</button></li>
+    </ul>
+    <br>
+    <input type="Submit"/>
+	
+    
+ 
+
+</body>
